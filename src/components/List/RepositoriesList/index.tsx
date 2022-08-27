e7e3090f07cacb494398848f0ac293affe044588
l-marcel <lmgh1312@gmail.com>
2022-08-27 19:57:41 -0300

    feat: listing all items

--- src/components/List/RepositoriesList/index.tsx
+++ src/components/List/RepositoriesList/index.tsx
@@ -1,41 +1,17 @@
-import { useWindowSize } from "@react-hook/window-size";
-import { MasonryScroller, useContainerPosition, usePositioner, useResizeObserver } from "masonic";
-import { useRef } from "react";
+import { Masonry } from "masonic";
 import { useFilteredRepositories } from "../../../context/hooks/useFilteredRepositories";
-import { usePagination } from "../../../context/hooks/usePagination";
 import { RepositoriesListItem } from "./RepositoriesListItem";
 
 
 export function RepositoriesList() {
-  const containerRef = useRef(null);
-  const { page } = usePagination();
   const { filteredRepositories } = useFilteredRepositories();
 
-  const baseIndex = page * 12;
-  const repositoriesInPage = filteredRepositories.slice(baseIndex, baseIndex + 16);
-
-  const [windowWidth, windowHeight] = useWindowSize();
-  const { offset, width } = useContainerPosition(containerRef, [
-    windowWidth,
-    windowHeight
-  ]);
-
-  const positioner = usePositioner(
-    { width, columnWidth: 360, columnGutter: 12 },
-    [repositoriesInPage]
-  );
-  
-  const resizeObserver = useResizeObserver(positioner);
-
   return (
-    <MasonryScroller
-      height={windowHeight}
-      offset={offset}
-      positioner={positioner}
-      containerRef={containerRef}
-      resizeObserver={resizeObserver}
-      overscanBy={6}
-      items={repositoriesInPage}
+    <Masonry
+      columnWidth={360}
+      columnGutter={12}
+      overscanBy={16}
+      items={filteredRepositories}
       render={({ index, data, width }) => (<RepositoriesListItem
         key={index}
         width={width}
