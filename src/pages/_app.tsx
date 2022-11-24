5c0d37b1537dd74ac0acac56b11c537024571b34
l-marcel <lmgh1312@gmail.com>
2022-11-24 20:13:49 -0300

    feat: adding vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,21 @@ import "react-vertical-timeline-component/style.min.css";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Layout } from "../styles/document/styles";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/main.scss";
 import "../styles/slider.scss";
 import "../styles/timeline.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Header/>
-      <Component {...pageProps} />
-      <Footer/>
-    </Layout>
+    <>
+      <Layout>
+        <Header/>
+        <Component {...pageProps} />
+        <Footer/>
+      </Layout>
+      <Analytics/>
+    </>
   );
 }
 
