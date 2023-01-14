05c9ddb94ede59cd21e28a1ca4b10bbb08ac3e0f
l-marcel <lmgh1312@gmail.com>
2023-01-14 14:19:12 -0300

    style: fixing demo video resolution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,7 +68,7 @@ function Resume({
         </ReactMarkdown>
         { demoVideoURL !== null && <DemoVideoContainer>
           <div className="absolute left-[3.75rem] h-full w-1 bg-primary-500"/>
-          <video className="2xç:max-h-[531px] z-10 border-r-4 border-primary-500 bg-white-500 dark:bg-gray-600 md:max-h-[354px] md:max-w-[630px] 2xl:max-w-[945px]" src={demoVideoURL} controls/>
+          <video className="bg-white-default dark:bg-gray-700 2xl:max-h-[531px] max-h-[440px] z-10 border-r-4 md:min-w-[400px] border-primary-500 bg-white-500 dark:bg-gray-600 md:max-h-[354px] md:max-w-[630px] 2xl:max-w-[945px]" src={demoVideoURL} controls/>
           <Image
             src="/assets/coding.svg"
             alt="A man coding..."
