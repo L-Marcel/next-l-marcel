3897fbae9198edff8405928e8f08f9081d3984ba
l-marcel <lmgh1312@gmail.com>
2023-01-14 21:28:44 -0300

    fix: changing pagination storage approach

--- src/context/hooks/usePaginationReducer.ts
+++ src/context/hooks/usePaginationReducer.ts
@@ -1,41 +1,57 @@
 import { useCallback, useEffect } from "react";
 import { Pagination } from "../providers/reducers/pagination";
 import create from "zustand";
-import { persist, redux } from "zustand/middleware";
+import { createJSONStorage, persist, redux } from "zustand/middleware";
 
-const useZustandPaginationReducer = create(persist(redux(Pagination.reducer, {
-  max: 0,
-  min: 0,
-  page: 0
-}), {
-  name: "l-marcel-pagination",
-  getStorage: () => sessionStorage,
-  version: 1
-}));
+const useZustandPaginationReducer = create(
+  persist(
+    redux(Pagination.reducer, {
+      max: 0,
+      min: 0,
+      page: 0,
+    }),
+    {
+      name: "l-marcel-pagination",
+      storage: createJSONStorage(() => {
+        return sessionStorage;
+      }),
+      version: 1,
+    }
+  )
+);
 
 interface UsePaginationReducerProps {
   size: number;
 }
 
 export function usePaginationReducer({ size }: UsePaginationReducerProps) {
-  const pagination = useZustandPaginationReducer((state) => state);
-  const dispatch = useZustandPaginationReducer((state) => state.dispatch);
+  const pagination = useZustandPaginationReducer((state) => {
+    return state;
+  });
+  const dispatch = useZustandPaginationReducer((state) => {
+    return state.dispatch;
+  });
 
-  const updatePageLimit = useCallback((min: number, max: number) => {
-    dispatch(Pagination.updateLimit(min, max));
-  }, [dispatch]);
+  const updatePageLimit = useCallback(
+    (min: number, max: number) => {
+      dispatch(Pagination.updateLimit(min, max));
+    },
+    [dispatch]
+  );
 
   useEffect(() => {
     const min = 0;
-    const max = Math.ceil(size/12) - 1;
-  
+    const max = Math.ceil(size / 12) - 1;
+
     updatePageLimit(min, max);
   }, []);
 
-  const setPage = useCallback((page: number, onError?: (page: number) => void) => {
-    dispatch(Pagination.setPage(page, onError));
-  }, [dispatch]);
-
+  const setPage = useCallback(
+    (page: number, onError?: (page: number) => void) => {
+      dispatch(Pagination.setPage(page, onError));
+    },
+    [dispatch]
+  );
 
   const nextPage = useCallback(() => {
     dispatch(Pagination.nextPage());
@@ -61,6 +77,6 @@ export function usePaginationReducer({ size }: UsePaginationReducerProps) {
     nextPage,
     lastPage,
     previousPage,
-    firstPage
+    firstPage,
   };
 }
